d53f73181c88620b1edd9cb780e9e024ed28e5df
mohsen naserian <mohsennaserian@gmail.com>
2025-08-13 11:08:05 -0700

    Config Web layers

---
+++ Src/Web/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Common;
+
+namespace Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : BaseApiController
+{
+}
